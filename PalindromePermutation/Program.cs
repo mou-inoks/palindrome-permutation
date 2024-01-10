5b0b3d7d5fd23ff72aeb8c0a3f5b767c1f3035b9
mou-inoks <mou@inokscapital.com>
2024-01-10 22:11:52 +0100

    first commit

---
+++ PalindromePermutation/Program.cs
@@ -0,0 +1,27 @@
+﻿bool IsPalindrome(string word1, string word2)
+{
+    bool isPalindrome = false;
+
+    if (word1.Length != word2.Length)
+        isPalindrome = false;
+    else
+    {
+        int t = word1.Length -1;
+        int v = 0;
+        for (int i = 0; i < word1.Length; i++)
+        {
+            if (word1[i] == word2[t])
+                v++;
+
+            t -= 1;
+        }
+
+        if (v == word1.Length)
+            isPalindrome = true;
+        
+    }
+
+    return isPalindrome;
+}
+
+Console.Write(IsPalindrome("salim","milal"));
